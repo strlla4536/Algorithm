242a6806cbe149f35a45cacba0e6d41c8b0acb83
strlla4536 <77538798+strlla4536@users.noreply.github.com>
2024-04-05 16:32:54 +0900

    [Bronze V] Title: 개수 세기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -4,4 +4,13 @@ input = sys.stdin.readline
 n = int(input())
 l= list(map(int,input().split()))
 v = int(input())
-print(l.count(v))
+
+cnt = 0 
+
+for i in range(len(l)):
+  if l[i] == v:
+    cnt += 1
+  else:
+    pass
+
+print(cnt)
