ead31ce59b2f21e95b01928e8e4f80f54523d14a
strlla4536 <77538798+strlla4536@users.noreply.github.com>
2024-04-12 19:22:40 +0900

    [Bronze II] Title: 나머지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,11 +1,6 @@
-import sys
-input = sys.stdin.readline
-
-arr = [0] * 43
+ans = set()
 
 for _ in range(10):
-  r = int(input()) % 42
-  arr[r] += 1
+  ans.add(int(input())%42)
 
-ans = [i for i in arr if i != 0]
 print(len(ans))
