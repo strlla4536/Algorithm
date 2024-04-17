664871ca1e8c96008bf45e6966aee043568ca341
strlla4536 <77538798+strlla4536@users.noreply.github.com>
2024-04-18 03:29:42 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,11 +1,4 @@
-# 알파벳 문자열 만들고 문자열에 있으면 인덱스로 접근해서 출력
-s = list(input())
-alphabets = 'abcdefghijklmnopqrstuvwxyz'
+s = input()
 
-for i in alphabets :
-  if i in s:
-    print(s.index(i), end=' ')
-
-  else:
-    print(-1, end=' ')
-
+for i in 'abcdefghijklmnopqrstuvwxyz':
+  print(s.find(i), end= ' ')
