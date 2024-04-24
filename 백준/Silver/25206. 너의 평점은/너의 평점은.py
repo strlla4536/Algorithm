4c281b1226d19c8fb4795bfd88c263d108e343a1
strlla4536 <77538798+strlla4536@users.noreply.github.com>
2024-04-24 18:09:20 +0900

    [Silver V] Title: 너의 평점은, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,39 +1,15 @@
+grades = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']  # 등급
+gpas = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0]  # 과목평점
 
-gpa_sum = 0  # 학점 총합
-gpa_times_grade = 0 # 과목별 학점x과목평점
+mul = 0 # 학점 x 과목평점
+time_sum = 0 # 학점 총합
 
 for i in range(20):
-  # 과목명, 학점, 등급(->과목평점)
-  #class, gpa, grade = 
-  li = list(map(str,input().split()))
-  if li[2] == 'P' :
-    pass
-  else:
-    gpa_sum += float(li[1]) 
+  # 과목, 학점, 등급
+  a, b, c = map(str, input().split())
+  if c in grades:
+    c = gpas[grades.index(c)]
+    time_sum += float(b)
+    mul += float(b) * float(c)
 
-  if li[2] == 'A+':
-    li[2] = 4.5
-  elif li[2] == 'A0':
-    li[2] = 4.0
-
-  elif li[2] == 'B+':
-    li[2] = 3.5
-  elif li[2] == 'B0':
-    li[2] = 3.0
-
-  elif li[2] == 'C+':
-    li[2] = 2.5
-  elif li[2] == 'C0':
-    li[2] = 2.0
-
-  elif li[2] == 'D+':
-    li[2] = 1.5
-  elif li[2] == 'D0':
-    li[2] = 1.0
-
-  else:
-    li[2] = 0
-
-  gpa_times_grade += float(float(li[1]) * li[2])
-
-print(gpa_times_grade / gpa_sum)
+print(mul / time_sum)
