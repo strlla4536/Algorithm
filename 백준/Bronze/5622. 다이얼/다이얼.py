ab4bf858f2f553151794e0380d82db3439b23ea9
strlla4536 <77538798+strlla4536@users.noreply.github.com>
2024-04-24 17:18:14 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,28 +1,15 @@
 # import sys
 # input = sys.stdin.readline
 
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+
 word = input()
 sum = 0
 
-for a in word:
-  
-  if a =='A' or a=='B' or a=='C':
-    sum += 3
-  elif a == 'D' or a=='E' or a=='F':
-    sum += 4
-  elif a == 'G' or a=='H' or a=='I':
-    sum += 5
-  elif a== 'J' or a=='K' or a=='L':
-    sum += 6
-  elif a == 'M' or a=='N' or a=='O':
-    sum += 7
-  elif a== 'P' or a=='Q' or a=='R' or a=='S':
-    sum += 8
-  elif a == 'T' or a=='U' or a=='V' :
-    sum += 9
-  elif a == 'W' or a=='X' or a=='Y' or a=='Z':
-    sum += 10
-
+for w in range(len(word)):
+  for i in dial:
+    if word[w] in i:
+      sum += dial.index(i) + 3
 
 
 print(sum)
