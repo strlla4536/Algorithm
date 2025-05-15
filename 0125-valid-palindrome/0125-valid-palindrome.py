4b04e0fdf36b1e5f4deeb0db7244455f12307192
strlla4536 <77538798+strlla4536@users.noreply.github.com>
2025-05-15 22:53:58 +0900

    Time: 7 ms (81.33%), Space: 19.5 MB (21.1%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -9,16 +9,15 @@ class Solution:
         if len(s)==0: return True
         
         start, end = 0, len(s)-1
-        i = 1
+        
         while s[start] == s[end]:
-            print(f"{i}번째")
-            print(f"update 전 start : {start}, end: {end}")
+            
             start += 1
             end -= 1
-            print(f"update 후 start : {start}, end: {end}")
+            
             if start >= end:
                 return True
-            i += 1
+            
             
         return False
         
