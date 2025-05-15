4b030f830eabbf3c8931ce5fd87335a29e123858
strlla4536 <77538798+strlla4536@users.noreply.github.com>
2025-05-15 22:58:57 +0900

    Time: 3 ms (98.9%), Space: 18.8 MB (32.17%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,23 +1,5 @@
 import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # s = re.sub('[-=+,#/\?:;^.@*\"※~ㆍ!』‘|\(\)\[\]`\'…》\”\“\’· _\{\}]', '',s).lower()
-        s = re.sub('\W', '', s).lower()
-        s = re.sub('[_]','',s)
-        print(s)
-
-        if len(s)==0: return True
-        
-        start, end = 0, len(s)-1
-        
-        while s[start] == s[end]:
-            
-            start += 1
-            end -= 1
-            
-            if start >= end:
-                return True
-            
-            
-        return False
-        
+        new = [i for i in s.lower() if i.isalnum()]
+        return new == new[::-1]
