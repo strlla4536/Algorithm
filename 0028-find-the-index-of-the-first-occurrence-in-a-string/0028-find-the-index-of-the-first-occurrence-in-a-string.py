0821128b156757e7e207ce0bf09930d1f65ca786
strlla4536 <77538798+strlla4536@users.noreply.github.com>
2025-05-15 22:19:23 +0900

    Time: 0 ms (100%), Space: 17.9 MB (32.27%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        # Sliding window
+        
+        if len(needle) > len(haystack): return -1
+
+        for i in range(len(haystack)):
+            if haystack[i] == needle[0]:
+                if haystack[i:i+len(needle)] == needle:
+                    return i
+
+        return -1
