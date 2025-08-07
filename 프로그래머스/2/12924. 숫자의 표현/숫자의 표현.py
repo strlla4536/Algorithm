12e9f885fe0c05d2b2a47a924b95fb0efc2d8d78
strlla4536 <77538798+strlla4536@users.noreply.github.com>
2025-08-08 02:04:49 +0900

    [level 2] Title: 숫자의 표현, Time: 6.43 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,14 @@
+def solution(num):
+    answer = 0
+    for x in range(1,num+1):
+        sum = 0
+        for y in range(x, num+1):
+            sum += y
+            if sum == num:
+                answer += 1
+                break
+            elif sum > num:
+                break
+
+    return answer
+
