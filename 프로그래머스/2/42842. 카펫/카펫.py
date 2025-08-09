a4b8301765bb169957b43f3ca7a17ed1f6562982
strlla4536 <77538798+strlla4536@users.noreply.github.com>
2025-08-09 19:43:46 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,35 +1,12 @@
-def check(brown, yellow, y_row, y_col):
-    a = brown + yellow
-    b = (y_row+2) * (y_col+2)
-    print("갈 + 노 = ",a)
-    print("가로 * 세로 = ", b)
-    if a % b == 0:
-        print("실행1")
-        return True
-    else:
-        print("실행2")
-        return False
-    
-    
 def solution(brown, yellow):
-    # (brown + yellow) = (yellow_row + 2) * (yellow_col + 2)
-    # yellow = yellow_row * yellow_col
-    if yellow == 1:
-        return [3, 3]
-    if yellow == 2:
-        return [4, 3]
-        
-    for r in range(1, yellow//2+1):
-        print("yellow row : ", r)
-        
-        if yellow % r == 0:
-            print("1")
-            c = yellow // r
-            print("c:", c)
-            
-            if r>=c:
-                print("brown, yellow row, yellow col = ", brown, r, c)
-                if check(brown, yellow, r, c):
-                    print("check:", check)
-                    return [r+2, c+2]
+    # 가로 x 세로 = 개수 => y_row * y_col = yellow
+    # 테두리 칸 개수 = 2*전체가로 + 2*전체세로 - 모서리4칸
+        # => 2 * (y_row + 2) + 2 * (y_col+2) - 4 = brown
+        # => 2*(y_row+y_col+2) = brown
+    
+    for y_row in range(1, int(yellow**(1/2))+1):
+        if yellow % y_row == 0:
+            y_col = yellow // y_row
+            if brown == 2*(y_row+y_col+2):
+                return [max(y_row, y_col)+2, min(y_row, y_col)+2]
     
